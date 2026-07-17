24912a848eae8c87b6bc4d31d75b40714c7267ab
jeremygoku <woutervanseveren@hotmail.com>
2026-07-17 15:20:40 +0200

    sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'tuinplanner-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Installatie: Sla bestanden op in de cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Bestanden worden gecachet');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// Activatie: Verwijder oude caches als er een nieuwe versie is
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+// Fetch: Laad uit cache indien beschikbaar, anders van netwerk
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
